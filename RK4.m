5379baeb43111776ac68401bc1afbfdad17393ce
Pameladlt <pamela_delatorrej@outlook.com>
2018-12-06 16:41:54 -0600

    Update RK4.m

--- RK4.m
+++ RK4.m
@@ -1,19 +1,27 @@
+%f=@(x,y) 2*x*y;
+%t0=0;
+%tf=2;
+%x0=1;
+%n=10;
+%[t,x] = RK4(f,t0,tf,x0,n)
+
+
 function [t,x] = RK4(f,t0,tf,x0,n)
 %Variable independiente t = Tiempo
 %Variable dependiente x con respecto el tiempo
-%F=EcuaciÛn diferencial que se ingresar·
+%F=Ecuaci√≥n diferencial que se ingresar√°
 %t0=Valor inicial de x
-%tf=Valor final de x (al que se llegar·)
-%x0=CondiciÛn inicial de la variable dependiente
-%n=N˙mero de intervalos que se har·n
+%tf=Valor final de x (al que se llegar√°)
+%x0=Condici√≥n inicial de la variable dependiente
+%n=N√∫mero de intervalos que se har√°n
 
-h=(tf-t0)/n; %tamaÒo de paso 
-disp('TamaÒo de Paso = '); disp(h)
-t=t0:h:tf; %desde t0(inicial) aumentar· con el tamaÒo de paso hasta llegar
+h=(tf-t0)/n; %tama√±o de paso 
+disp('Tama√±o de Paso = '); disp(h)
+t=t0:h:tf; %desde t0(inicial) aumentar√° con el tama√±o de paso hasta llegar
 %a tf(final)
-x=zeros(n+1,1);%se almacenar·n los datos de las iteraciones aqui
+x=zeros(n+1,1);%se almacenar√°n los datos de las iteraciones aqui
 disp('Datos de irteraciones almacenados= '); disp(x)
-x(1)=x0; %la primera iteraciÛn ser· con el valor inicial
+x(1)=x0; %la primera iteraci√≥n ser√° con el valor inicial
 
 %ciclo for por ciclo iterativo
 for i=1:n %hasta n iteraciones n(valor dado)

61b2a7a1b46dc5f5be72aa1e989e66ab5e0c7726
willi556 <william_chuy@live.com.mx>
2018-12-06 12:33:12 -0600

    Update Cholesky.m

--- Cholesky.m
+++ Cholesky.m
@@ -6,10 +6,12 @@ function x = Cholesky(A)
 [m,n]=size(A);
 if m~=n, error('NO SE PUEDE USAR ESTE METODO CON UNA MATRIZ MxN, TIENE QUE SER CUADRADA'); end
 
-b=[sum(A(1,:)); sum(A(2,:)); sum(A(3,:))];
+b=[sum(A(1,:)); sum(A(2,:)); sum(A(3,:))]; %Para el caso que no nos den una b
 
 U=chol(A);      %Nos dan una matriz A y con este comando nos da la matriz R (triangulo superior)
 disp('U= '); disp(U)
+
+%U^ty=b
 d=U'\b; %Para poder sacar los valores de d=Y
 x=U\d; %Se despeja X, y eso hace que U(R) sea divida entre d(Y)
 disp('x = '); disp(x)

b2c7ed43b5896df5c6fc73d2e7eb0c4869810faa
lauralcrz <45669115+lauralcrz@users.noreply.github.com>
2018-12-06 12:25:01 -0600

    Add files via upload

---
+++ m_biseccion.m
@@ -0,0 +1,64 @@
+% Método de la bisección:
+% 1. Selecciona un intervalo [a,b]
+% 2. Calculo del punto medio como nuevo punto
+% 3. Revisión de cambio de signo, producto f(a)·f(b)
+% 4. Si el producto es cero, entonces tenemos una raíz,
+%    sino se repite el proceso a partir del paso 2.
+
+% En esta función se deben ingresar los siguientes datos:
+% - la función a evaluar (f),
+% - el límite superior (a) e inferior (b),
+% - y la tolerancia de error posible (e).
+
+function [y] = m_biseccion(f,a,b,e)
+
+clc 
+
+format long 
+
+% Se debe establecer a "f" como una funcion a partir de una entrada str,
+% agregando un @(x) antes de la función en la que se evaluara
+fh = str2func(f);
+
+% Se inicializan valores para valores y datos auxiliares,
+% estos últimos en caso de ser requeridos.
+c = 0;
+n = 0; % n: número de iteración
+e_max_ps = (b-a)/2;
+
+% Para apreciar las iteraciones y los resultados que 
+% se van obteniendo durante el proceso,
+% se hace el siguiente arreglo:
+fprintf('\t n \t\ta \t\t    c \t\t        b \t\t        Error\n') 
+% \t para tabulación, \n para salto de línea
+
+% Para saber si continuan las iteraciones, se comparan
+% los valores del maximo error posible (e_max_ps)
+% con la tolerancia de error ingresada (e).
+while (e_max_ps > e)
+    c = (a+b)/2;
+    disp([n,a,c,b,e_max_ps]) 
+    % De esta manera creamos un vector fila, es decir, los datos se
+    % acomodan en un renglón
+    if ( fh(a)*fh(c) < 0) 
+        % Con esta condición, evaluamos el cambio de signo, 
+        % es decir, si el producto es menor a 0, se tiene un numero negativo, 
+        % por lo tanto el valor obtenido en "c", será el nuevo valor para "b", 
+        % y por otro lado, si da un numero positivo, el numero nuevo será 
+        % el valor de "a".
+        b = c;
+    else a = c;
+    end
+    e_max_ps = (b-a)/2;
+    n = n+1;
+end
+
+fprintf('Raiz encontrada con una tolerancia de %f:\n\t%f\n', e, c)
+y = c;
+
+
+
+
+
+
+
---
+++ m_int_lagrange.m
@@ -0,0 +1,50 @@
+% Interpolación de Lagrange
+% En esta función se ingresan en forma vectorial los puntos a evaluar,
+% de tal forma que se devuelven los coefecientes del polinomio,
+% recordando que de derecha a izquierda, se comienza con el término 
+% independiente (x^0 = 1) y de ahí se va aumentado el exponencial 
+
+function [C] = m_int_lagrange(x,y) 
+
+clc
+
+p = length(x); % Cantidad de puntos, en donde tambien se podría tomar
+               % la longitud de y, pues tienen el mismo tamaño de vector.
+n = p-1; % Grado del polinomio 
+
+% Para auxiliarnos en el calculo, se va a crear una matriz de 0,
+% donde se van a ir almacenando los coeficientes del polinomio de Lagrange.
+L = zeros(p,p);
+
+% Procedimiento
+for k = 1:n+1
+    i = 1; 
+    % Parametro de ayuda para multiplicar todos los factores de 
+    % los coeficientes de Lagrange, en el primer caso sería 1 multiplicado 
+    % por (x)*(x-1), luego para k = 2 sería 1 por (x-1)*(x-2) 
+    % para k=3 1 por (x-1)*(x-2)*(x-3) y así sucevimanete hasta llegar a n+1.
+    for j = 1:n+1;
+        % Ya que todos los numeradores y denominadores tienen el "mismo
+        % valor", sin embargo la única diferencia es que k y j no pueden
+        % ser iguales en el denominador, entonces se estable una condición
+        if k ~= j
+            i = conv(i,poly(x(j)))/(x(k)-x(j));
+            % Aquí lo que estamos haciendo es seguir la formula
+            % Para la multiplicación de polinomios se utiliza el comando
+            % conv
+            % El polinimo de xj, o sea, si j vale 1, sería x-x1, luego para j=2 sería 1 por x-x1 por 1*x-1x-2   
+            % dividido entre x(k) - x(j), aquí es donde observamos que si k
+            % y j fueran iguales en el denominador no se podría llevar
+            % acabo ya que daría 0.
+        end 
+    end
+    
+    % Actualizamos matriz L 
+    L (k,:) = i  % Con esto hacemos que la fila k sea igual a lo que encontramos en i
+end 
+
+% Por último hacemos una combinación lineal, es decir,
+% lo que nos indica P(x) = y0(l0) + y1(l1)...
+C = y*L
+
+

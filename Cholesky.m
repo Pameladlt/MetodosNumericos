21b27bae712344076e69fe101c308fbbc0b6ee02
willi556 <william_chuy@live.com.mx>
2018-12-06 19:30:17 -0600

    Update Cholesky.m

--- Cholesky.m
+++ Cholesky.m
@@ -19,15 +19,16 @@ function x = Cholesky(A)
 %por medio de comandos hacemos la solucion de ecuaciones
 %despejamos para obtener los resultados
 
+function x = Cholesky(A,b)
+
+%Método de Cholesky solo funciona con matrices cuadradas
+%[nxn]
+%Formula Rx=Y
 [m,n]=size(A);
 if m~=n, error('NO SE PUEDE USAR ESTE METODO CON UNA MATRIZ MxN, TIENE QUE SER CUADRADA'); end
 
-b=[sum(A(1,:)); sum(A(2,:)); sum(A(3,:))]; %Para el caso que no nos den una b
-
 U=chol(A);      %Nos dan una matriz A y con este comando nos da la matriz R (triangulo superior)
 disp('U= '); disp(U)
-
-%U^ty=b
 d=U'\b; %Para poder sacar los valores de d=Y
 x=U\d; %Se despeja X, y eso hace que U(R) sea divida entre d(Y)
 disp('x = '); disp(x)
